22767c5bdbba5901fde0ed3ed9ed04b6f54ac754
Moin-A <43196743+Moin-A@users.noreply.github.com>
2020-09-27 10:50:43 +0530

    added Movie.jsx

---
+++ src/components/movies.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class Movies extends Component {
+  state = {};
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Genre</td>
+            <td>Stock</td>
+            <td>Rate</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>majnxja</td>
+            <td>anxjanx</td>
+            <td>ajnxja</td>
+            <td>xajn</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Movies;
